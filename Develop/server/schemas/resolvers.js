97ec064e69984ca580d233474b50a56703cb51f4
SindidR <sindidreedh@gmail.com>
2022-12-07 18:18:43 -0500

    resolvers-done

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,30 +1,17 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        users: async () => {
-            return User.find().populate('thoughts');
-        },
-        user: async (parent, { username }) => {
-            return User.findOne({ username }).populate('thoughts');
-        },
-        thoughts: async (parent, { username }) => {
-            const params = username ? { username } : {};
-            return Thought.find(params).sort({ createdAt: -1 });
-        },
-        thought: async (parent, { thoughtId }) => {
-            return Thought.findOne({ _id: thoughtId });
-        },
-        me: async (parent, _, context) => {
+        me: async (parent, { username, email, password }, context) => {
             if (context.user) {
-                return User.findOne({ _id: context.user._id }).populate('thoughts');
+                const userData = await User.findOne({ _id: context.user._id }).select('-__ -password');
+                return userData;
             }
             throw new AuthenticationError('You need to be logged in!');
-        },
+        }
     },
-
     Mutation: {
         addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
@@ -48,31 +35,12 @@ const resolvers = {
 
             return { token, user };
         },
-        addThought: async (parent, { thoughtText }, context) => {
-            if (context.user) {
-                const thought = await Thought.create({
-                    thoughtText,
-                    thoughtAuthor: context.user.username,
-                });
 
-                await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $addToSet: { thoughts: thought._id } }
-                );
-
-                return thought;
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
-        addComment: async (parent, { thoughtId, commentText }, context) => {
+        saveBook: async (parent, { newBook }, context) => {
             if (context.user) {
                 return Thought.findOneAndUpdate(
-                    { _id: thoughtId },
-                    {
-                        $addToSet: {
-                            comments: { commentText, commentAuthor: context.user.username },
-                        },
-                    },
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: newBook } },
                     {
                         new: true,
                         runValidators: true,
@@ -81,32 +49,14 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!');
         },
-        removeThought: async (parent, { thoughtId }, context) => {
-            if (context.user) {
-                const thought = await Thought.findOneAndDelete({
-                    _id: thoughtId,
-                    thoughtAuthor: context.user.username,
-                });
 
-                await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $pull: { thoughts: thought._id } }
-                );
-
-                return thought;
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
-        removeComment: async (parent, { thoughtId, commentId }, context) => {
+        removeComment: async (parent, { bookId }, context) => {
             if (context.user) {
                 return Thought.findOneAndUpdate(
-                    { _id: thoughtId },
+                    { _id: context.user._id },
                     {
                         $pull: {
-                            comments: {
-                                _id: commentId,
-                                commentAuthor: context.user.username,
-                            },
+                            savedBooks: { bookId }
                         },
                     },
                     { new: true }
